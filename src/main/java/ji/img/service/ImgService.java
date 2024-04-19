41b06297427befee2c552dc5d84a96afff4284be
shinjyun <shinproject21@gmail.com>
2024-04-19 12:17:57 +0900

    최신화 이후의 push

---
+++ src/main/java/ji/img/service/ImgService.java
@@ -0,0 +1,14 @@
+package ji.img.service;
+
+import java.util.ArrayList;
+
+import ji.img.dto.ImgDTO;
+
+public interface ImgService {
+	public ArrayList<ImgDTO> imgSelectAll();
+	
+	public ImgDTO imgSelect(int img_number);
+	public ImgDTO imgInsert(ImgDTO imgDTO);
+	public ImgDTO imgUpdate(ImgDTO imgDTO);
+	public ImgDTO imgDelete(int img_number);
+}
