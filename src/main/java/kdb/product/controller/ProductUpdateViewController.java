82aa2c7340b7ed49cdb967100323d6900dcdb26a
JunSuU98 <qoreh1216@gmail.com>
2024-04-22 16:16:27 +0900

    fix: 상품 내용을 수정하면 기존의 상품 정보가 날아가고, 수정한 내용도 제대로 입력되지 않는 오류 수정

--- src/main/java/kdb/product/controller/ProductUpdateViewController.java
+++ src/main/java/kdb/product/controller/ProductUpdateViewController.java
@@ -15,22 +15,27 @@ public class ProductUpdateViewController implements Controller {
 	public ProductHandlerAdapter execute(HttpServletRequest request, HttpServletResponse response) {
 		String product_title = request.getParameter("product_title");
 		log.info(product_title);
-		String product_conten = request.getParameter("product_conten");
+		String product_content = request.getParameter("product_content");
 		String prdouct_status  = request.getParameter("product_status");
 		int product_price = Integer.parseInt(request.getParameter("product_price"));
 		log.info(product_price);
-		  int product_number = Integer.parseInt(request.getParameter("product_number"));
+		int product_number = Integer.parseInt(request.getParameter("product_number"));
+		
 		ProductDAO productDAO= new ProductDAO();
 		ProductDTO productDTO = new ProductDTO();
+		
 		productDTO.setProduct_title(product_title);
-		productDTO.setProduct_content(product_conten);
+		productDTO.setProduct_content(product_content);
 		productDTO.setProduct_status(prdouct_status);
 		productDTO.setProduct_price(product_price);
 		productDTO.setProduct_number(product_number);
+		
 		productDTO= productDAO.productUpdate(productDTO);
 		request.setAttribute("productDTO", productDTO);
+		
 		ProductHandlerAdapter productHandlerAdapter = new ProductHandlerAdapter();
 		productHandlerAdapter.setPath("/WEB-INF/view/product_view/product_update_view.jsp");
+		
 		return productHandlerAdapter;
 	}
 
